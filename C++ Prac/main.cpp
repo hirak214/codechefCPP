81703e815f30f40d8fc3591e2af6edd0fdf5e514
Hirak Desai <hirak214@gmail.com>
2019-07-16 12:02:10 +0530

    Initial Commit

---
+++ C++ Prac/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++ Prac
+//
+//  Created by Hirak Desai on 16/07/19.
+//  Copyright © 2019 Hirak Desai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
